415579b8d74eee0657bb5f62ff2e139eb75d5035
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-18 21:57:30 +0200

    stage 6 completed

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,21 +1,20 @@
 import random
 
-from nltk.tokenize import word_tokenize
-
 
 def load_corpus(file_name):
     f = open(file_name, 'r', encoding="utf-8")
     tokens = []
     for line in f:
         tokens.extend(line.split())
-        # print("+")
-    # tokens = word_tokenize(f.read().lower())
-    # print(tokens[:100])
     return tokens
 
 
 def get_bigrams(tokens):
     return list(zip(tokens[:-1], tokens[1:]))
+
+
+def get_trigrams(tokens):
+    return [(x + " " + y, z) for x, y, z in zip(tokens[:-2], tokens[1:-1], tokens[2:])]
 
 
 def get_markov_dict(tokens):
@@ -35,69 +34,49 @@ def get_markov_dict(tokens):
 
 def generate_random_sentence(markov_dict, max_length):
     words = []
-    # first_word = ""
-    while True:
+    notfound = True
+    while notfound:
         first_word = random.choice(list(markov_dict.keys()))
-        if first_word[0].isupper() and first_word[-1] not in ['.', '!', '?']:
-            break
+        notfound = False
+        if first_word[0].isupper():
+            for c in ['.', '!', '?']:
+                if first_word.find(c) != -1:
+                    notfound = True
+                    break
+        else:
+            notfound = True
 
-    words.append(first_word)
-    # print(word)
-    # print(markov_dict[word].keys())
-    word = first_word
-    for _ in range(max_length - 1):
-        # print("----")
+    words.extend(first_word.split())
+
+    while len(words) <= max_length - 1:
+        word = " ".join(words[-2:])
         next_list = list(markov_dict[word].keys())
         next_counts = list(markov_dict[word].values())
 
         word = random.choices(next_list, next_counts)[0]
-        words.append(word)
-        # print("1**",words)
+        words.extend(word.split())
 
-    while word[-1] not in ['.', '!', '?']:
+    while words[-1][-1] not in ['.', '!', '?']:
+        word = " ".join(words[-2:])
         next_list = list(markov_dict[word].keys())
         next_counts = list(markov_dict[word].values())
 
         word = random.choices(next_list, next_counts)[0]
-        words.append(word)
-        # print("2**",words)
+        for w in word.split():
+            words.append(w)
+            if w[-1] in ['.', '!', '?']:
+                break
 
-        # print(word)
-        # print(markov_dict[word].keys())
-    # word = random.choice(list(markov_dict[word].keys()))
-    # words.append(word)
     return words
 
 
 def main():
     corpus_file_name = input()
     tokens = load_corpus(corpus_file_name)
-    bigrams = get_bigrams(tokens)
-    # print(f"Number of bigrams: {len(bigrams)}")
-    markov_dict = get_markov_dict(bigrams)
+    grams = get_trigrams(tokens)
+    markov_dict = get_markov_dict(grams)
     for _ in range(10):
         sent = generate_random_sentence(markov_dict, 5)
         print(" ".join(sent))
-        # # print("**")
-        # user_input = input()
-        #
-        # if user_input == "exit":
-        #     break
-        # try:
-        #     head = user_input
-        #     # print(f"**{head}**")
-        #     selected = markov_dict[head]
-        #     print(f"Head: {head}")
-        #     for k in selected:
-        #         print(f"Tail: {k}     Count: {selected[k]}")
-        # except TypeError:
-        #     print("Type Error. Please input an integer.")
-        # except ValueError:
-        #     print("Value Error. Please input an integer.")
-        # except IndexError:
-        #     print("IndexError: list index out of range")
-        # except KeyError:
-        #     print("Key Error. The requested word is not in the model. Please input another word.")
-
 
 main()
